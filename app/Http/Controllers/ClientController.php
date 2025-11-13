5f4b51f1db7f5ac0d2a1d90c165a77c10994e7d4
jesusmedina262 <medinaaguilarjesusdavid@gmail.com>
2025-11-13 00:53:15 -0500

    Add index method to ClientController
    Fetch clients with their document types for the index view.

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -7,5 +7,9 @@ use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
-    //
+    public function index()
+    {
+        $clients = Client::with('type_document')->get();
+        return view('Dashboard.client.Index', compact('client'));
+    }
 }
