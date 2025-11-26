2dd02b7f2323d97b9aedef144fc3b3511a57d719
KeniaCardenas01 <kenianairobicardenasnegron@gmail.com>
2025-11-26 18:39:37 -0500

    Update ProviderUpdateRequest.php

--- app/Http/Requests/Providers/ProviderUpdateRequest.php
+++ app/Http/Requests/Providers/ProviderUpdateRequest.php
@@ -29,8 +29,7 @@ class ProviderUpdateRequest extends FormRequest
             'name'=>['required','string','max:100'],
             'phone'=>['nullable','regex:/^[0-9]+$/','max:20'],
             'address'=>['nullable','string','max:150'],
-            'email'=>['nullable','email','max:100'],
-
+            'email' => ['required', 'email:rfc', 'regex:/^[\w\.\-]+@([\w\-]+\.)+[a-zA-Z]{2,}$/'],
         ];
     }
     public function attributes()
