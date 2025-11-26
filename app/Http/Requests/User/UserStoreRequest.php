aa70ddaf648568351b43f56c737789f135869e4f
KeniaCardenas01 <kenianairobicardenasnegron@gmail.com>
2025-11-26 18:40:10 -0500

    Update UserStoreRequest.php

--- app/Http/Requests/User/UserStoreRequest.php
+++ app/Http/Requests/User/UserStoreRequest.php
@@ -34,7 +34,7 @@ class UserStoreRequest extends FormRequest
             'document_number' => ['required', 'string', 'min:5', 'max:20', 'unique:users'],
             'phone_number' => ['required', 'string', 'size:10'],
             'address' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'email', 'max:255', 'unique:users'],
+            'email' => ['required', 'email:rfc', 'regex:/^[\w\.\-]+@([\w\-]+\.)+[a-zA-Z]{2,}$/','unique:users'],
             'password' => ['required', 'string', 'min:6', 'confirmed'],
             'password_confirmation' => ['required', 'string', 'min:6'],
             'title' => ['required', 'string', Rule::in($this->titles())],
